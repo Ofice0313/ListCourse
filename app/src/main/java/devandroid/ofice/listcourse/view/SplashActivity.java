27ad033b1450b6418b37ef35bc2a15c339dde451
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2024-11-03 19:39:56 +0200

    creating the principal window

---
+++ app/src/main/java/devandroid/ofice/listcourse/view/SplashActivity.java
@@ -0,0 +1,43 @@
+package devandroid.ofice.listcourse.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import devandroid.ofice.listcourse.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int TIME_OUT_SPLASH = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        cumutarTelaSplash();
+    }
+
+    private void cumutarTelaSplash() {
+        new Handler().postAtTime(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_OUT_SPLASH);
+    }
+}
